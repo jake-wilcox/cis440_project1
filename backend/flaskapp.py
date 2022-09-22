51c3ab0c8b28f96942425c5433e9890f4077489a
Jake Wilcox <jakewilcox242@gmail.com>
2022-09-21 19:59:33 -0700

    Setup Virtual env, flask+sql

---
+++ backend/flaskapp.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, render_template
+from flask_mysqldb import MySQL
+
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
+
+
+
+app.config['MYSQL_HOST'] = '107.180.1.16'
+app.config['MYSQL_USER'] = '440fall20225'
+app.config['MYSQL_PASSWORD'] = '440fall20225'
+app.config['MYSQL_DB'] = '440fall20225'
+
+mysql = MySQL(app)
+
+
+@app.route('/')
+def home():
+    return 'Home Page'
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    if request.method == "POST":
+        print('called')
+        cursor = mysql.connection.cursor()
+        print('cursor created')
+        cursor.execute('SELECT * FROM user')
+        account = cursor.fetchone()
+        return f'{account}'
+    
+    else:
+        return html_string
+    
+
+
+
+
+html_string = "<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'/><title>Kool Gamez</title><link href='styles.css' rel='stylesheet'/></head><body><form action='#' method='POST'><p>Click to view user database</p><input type='submit' value='submit'></form></body></html>"
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ commonQuerys.sql
@@ -0,0 +1,10 @@
+/*Adding rows to table*/
+/*  INSERT INTO user (username, password, totalScore) VALUES (jake, jakewilcox, 0)  */
+
+
+/*Updating a value- can be used for score*/
+UPDATE user
+SET totalScore = 5
+WHERE id = '1';
+
+
